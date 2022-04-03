a1badf3949a340849da4d21ce15e889ef4892317
Irina <myr-irina2@yandex.ru>
2022-04-03 07:17:57 +0300

    added slider scroll

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,6 @@
 import "./Slider.css";
 import User from "../User/User";
+import { ITEM_WIDTH } from "./../../utils/constants";
 
 import { useCallback, useState, useEffect, useRef } from "react";
 import {
@@ -8,44 +9,54 @@ import {
 	CARDS_FOR_MIN_WIN_SIZE,
 } from "./../../utils/constants";
 import { transpile } from "typescript";
+import { func } from "prop-types";
 
 function Slider({ users }) {
-	// const windowWidth = window.innerWidth;
-	// const [cardsQuantity, setCardsQuantity] = useState(0);
+	const [offSet, setOffset] = useState(0);
 
-	// const renderCards = useCallback(() => {
-	// 	if (windowWidth > 933) {
-	// 		setCardsQuantity(CARDS_FOR_MAX_WIN_SIZE);
-	// 	} else if (windowWidth > 475 && windowWidth < 933) {
-	// 		setCardsQuantity(CARDS_FOR_MEDIUM_WIN_SIZE);
-	// 	} else {
-	// 		setCardsQuantity(CARDS_FOR_MIN_WIN_SIZE);
-	// 	}
-	// }, [windowWidth]);
+	function handleLeftArrowClick() {
+		console.log("handleLeftArrowClick");
 
-	// useEffect(() => renderCards(), [renderCards]);
+		setOffset((currentOffset) => {
+			const newOffset = currentOffset + ITEM_WIDTH;
 
-	const slider = useRef();
+			console.log(newOffset);
+			return Math.min(newOffset, 0);
+		});
+	}
 
-	// function prevHandler() {
-	// 	console.log(slider)
-	// 	slider.current.childNodes.map((element) => {
-	// 		element.style={transform: translateX(200)}
-	// 	});
-	// }
+	function handleRightArrowClick() {
+		console.log("handleRightArrowClick");
 
-	// function nextHandler() {
-	// 	console.log("next");
-	// }
+		setOffset((currentOffset) => {
+			console.log(currentOffset)
+			const newOffset = currentOffset - ITEM_WIDTH;
+
+			const maxOffset = -(ITEM_WIDTH * (users.length - 1));
+			return Math.max(newOffset, maxOffset);
+		});
+	}
 
 	return (
 		<div className='main__container'>
 			<div className='window'>
 				<div className='arrow'>
-					<button className='button-left' ></button>
-					<button className='button-right' ></button>
+					<button
+						className='button-left'
+						onClick={handleLeftArrowClick}
+					></button>
+					<button
+						className='button-right'
+						onClick={handleRightArrowClick}
+					></button>
 				</div>
-				<section className='all-items-container' ref={slider}>
+				<section
+					className='all-items-container'
+					// ref={slider}
+					style={{
+						transform: `translateX(${offSet}px)`,
+					}}
+				>
 					{users &&
 						users.map((user, id) => {
 							return <User user={user} key={id} />;
